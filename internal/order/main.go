d25782591342f7ac7e9204026acb571ebad35740
张柏川 <1641671577@qq.com>
2024-12-31 10:35:07 +0800

    update lesson3&4

--- internal/order/main.go
+++ internal/order/main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
+	"github.com/ZBCccc/gorder-v2/common/config"
+	"github.com/spf13/viper"
 	"log"
-	"net/http"
 )
 
-func main() {
-	log.Println("Listening on port 8082")
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("<h1>Hello, World!</h1>"))
-		if err != nil {
-			return
-		}
-	})
-
-	if err := http.ListenAndServe(":8082", mux); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+func init() {
+	if err := config.NewViperConfig(); err != nil {
+		log.Fatal(err)
 	}
+}
+func main() {
+	log.Println(viper.Get("order.service-name"))
 }
